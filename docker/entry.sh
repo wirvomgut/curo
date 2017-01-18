88a9fffa8cc3644ef9d8004ff7e7d3b1ce8009c2
Julian Liebl <jujulian1987@gmail.com>
2017-01-18 15:11:50 +0100

    fixed config file reference property

--- docker/entry.sh
+++ docker/entry.sh
@@ -2,4 +2,4 @@
 
 rm /curo/RUNNING_PID
 rm -R /curo/db
-/curo/bin/curo -Dplay.crypto.secret=${PLAY_SECRET} -Dplay.evolutions.db.default.autoApply=true -Dconfig.resource=/conf/curo.conf
+/curo/bin/curo -Dplay.crypto.secret=${PLAY_SECRET} -Dplay.evolutions.db.default.autoApply=true -Dconfig.file=/conf/curo.conf
