cecd3b0c8d961578a83532d06dca9092c3f85a0c
Julian Liebl <jujulian1987@gmail.com>
2017-03-13 19:52:14 +0100

    added option for external links

---
+++ app/models/config/Links.scala
@@ -0,0 +1,16 @@
+package models.config
+
+import play.api.{Configuration, Play}
+
+/**
+  * Created by julianliebl on 13.03.17.
+  */
+case class Link(url: String, path: Option[String] = None) {
+  val target: String = if(path.isDefined) "_self" else "_blank"
+}
+
+object Links  {
+  private val config: Configuration = Play.configuration(Play.current)
+
+  val forum: Link = Link(config.getString("app.discourse.url").get, config.getString("app.discourse.path"))
+}
