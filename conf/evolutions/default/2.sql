58d642542fe901e6356506f172317f4ddd87d7bb
razem-io <jujulian1987@gmail.com>
2017-05-20 21:57:44 +0200

    added sql primary keys and added date created for coin entry

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -1,19 +1,20 @@
 # --- !Ups
 
 CREATE TABLE persons (
-    id int NOT NULL AUTO_INCREMENT,
+    id INT AUTO_INCREMENT primary key NOT NULL,
     uid varchar(255) NOT NULL UNIQUE
 );
 
 CREATE TABLE work_entries (
-    id int NOT NULL AUTO_INCREMENT,
+    id INT AUTO_INCREMENT primary key NOT NULL,
     person_id varchar(255) NOT NULL,
     area varchar(255) NOT NULL,
     task varchar(255) NOT NULL,
     description varchar(255) NOT NULL,
     time_spent int NOT NULL,
     coins double NOT NULL,
-    date_done date NOT NULL
+    date_done date NOT NULL,
+    date_created DATETIME NOT NULL DEFAULT(GETDATE())
 );
 
 # --- !Downs
